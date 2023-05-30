bb74d229ed38a385f81e0714b67efec8bd1f4400
ansarthameem30 <57943142+ansarthameem30@users.noreply.github.com>
2023-05-30 14:58:13 +0530

    .
    l

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 
-import './App.css';
-import {motion} from 'framer-motion'
+
 
 function App() {
   return (
-    <div className="App">
-      <motion.div
-      className="box"
-      whileHover={{scale:1.1}}
-      whileTap={{scale:0.8}}
-    />
-       
-    </div>
+    <>
+    </>
   );
 }
 
